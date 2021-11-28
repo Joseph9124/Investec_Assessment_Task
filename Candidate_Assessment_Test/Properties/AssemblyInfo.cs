d0745ce1e32fb1193498bef5c5b58724ce064498
Joseph9124 <ojaymatthews@yahoo.com>
2021-11-28 13:14:33 +0200

    Ivestec candidate assessment

---
+++ Candidate_Assessment_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Candidate_Assessment_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Candidate_Assessment_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89d20383-3290-4983-98ea-a5f38f39fdbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Candidate_Assessment_Test/StringManupulationTest.cs
@@ -0,0 +1,119 @@
+﻿using Candidate_Assessment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Candidate_Assessment_Test
+{
+    [TestClass]
+    public class StringManupulationTest
+    {
+        [TestMethod]
+        public void TestDuplicates()
+        {
+            String str = "I like to eat apples";
+            String options = "1";
+
+            StringManupulation sm = new StringManupulation(str,options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestDuplicatesMultipleOptions()
+        {
+            String str = "I like to eat apples";
+            String options = "12";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestDuplicatesMultipleOptionsTwo() {
+            String str = "I like to eat apples";
+            String options = "123";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestDuplicatesTwo()
+        {
+            String str = "I like eating apples";
+            String options = "1";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestDuplicatesThree()
+        {
+            String str = "abcd4";
+            String options = "1";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestVowels()
+        {
+            String str = "I like eating apples";
+            String options = "2";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestVowelsTwo()
+        {
+            String str = "jkl kkjh";
+            String options = "2";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestVowelsAndNonVowels()
+        {
+            String str = "I eat";
+            String options = "3";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestVowelsAndNonVowelsTwo()
+        {
+            String str = "that dog";
+            String options = "3";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+
+        [TestMethod]
+        public void TestVowelsAndNonVowelsThree()
+        {
+            String str = "3 a";
+            String options = "3";
+
+            StringManupulation sm = new StringManupulation(str, options);
+
+            sm.performOperations();
+        }
+    }
+}
